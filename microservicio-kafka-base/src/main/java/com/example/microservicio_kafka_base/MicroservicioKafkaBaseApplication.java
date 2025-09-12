ed9b5b0085c6541bfb769c1b5bf1c7ac2a542323
Rubén Soria <rusok.software@gmail.com>
2025-09-12 19:59:50 -0400

    Primer commit

---
+++ microservicio-kafka-base/src/main/java/com/example/microservicio_kafka_base/MicroservicioKafkaBaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservicio_kafka_base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioKafkaBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioKafkaBaseApplication.class, args);
+	}
+
+}
